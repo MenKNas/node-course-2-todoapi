ecbdf2a8e03379cf16a448139de5ea5d40c8275f
Menelaos <men.nasies@gmail.com>
2018-05-04 15:01:20 +0300

    heroku deployment fix

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,9 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise ;
-mongoose.connect("mongodb://Menios:19911991>@ds115420.mlab.com:15420/node-course-note-api");
+//port configuration for local db/prod db
+const url = process.env.DATABASEURL || "mongodb://localhost:27001";
+mongoose.connect("mongodb://Menios:19911991@ds115420.mlab.com:15420/node-course-note-api");
 
 module.exports = {
     mongoose:mongoose
