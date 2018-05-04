a5778e7a9fc29af4f678f5a19c0e4a25b9d6a846
Menelaos <men.nasies@gmail.com>
2018-05-04 15:17:26 +0300

    database fix

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise ;
 //port configuration for local db/prod db
-const url = process.env.DATABASEURL || "mongodb://localhost:27001";
+const url = "mongodb://Menios:19911991@ds115420.mlab.com:15420/node-course-note-api"
 mongoose.connect(url);
 
 module.exports = {
